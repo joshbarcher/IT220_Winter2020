b71b9b71308d4eee56ad8eb1f06f827613da97f0
Josh Archer <jarcher@greenriver.edu>
2020-01-07 14:52:42 -0800

    Added an example class that uses the "this" keyword in different ways.

---
+++ oop_review/src/this_keyword/NinjaTurtle.java
@@ -0,0 +1,54 @@
+package this_keyword;
+
+public class NinjaTurtle
+{
+    private String bandanaColor;
+    private String weapon;
+    private String name;
+
+    public NinjaTurtle()
+    {
+        //this is called constructor chaining
+        this("Blue", "Katana", "Leonardo");
+    }
+
+    public NinjaTurtle(String name)
+    {
+        //System.out.println(name);
+
+        //calling this() must be the first line of code in a constructor
+        this("blue", "katana", name);
+    }
+
+    public NinjaTurtle(String bandanaColor, String weapon, String name)
+    {
+        this.bandanaColor = bandanaColor;
+        this.weapon = weapon;
+        this.name = name;
+    }
+
+    public String getBandanaColor()
+    {
+        return bandanaColor;
+    }
+
+    public String getWeapon()
+    {
+        return weapon;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "NinjaTurtle{" +
+                "bandanaColor='" + bandanaColor + '\'' +
+                ", weapon='" + weapon + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
