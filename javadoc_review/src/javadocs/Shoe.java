582ada7976cb72ef236f073116d41803acda3d4b
Josh Archer <jarcher@greenriver.edu>
2020-02-13 14:02:48 -0800

    Added the remaining parts of our Javadoc review.

--- javadoc_review/src/javadocs/Shoe.java
+++ javadoc_review/src/javadocs/Shoe.java
@@ -1,12 +1,13 @@
 package javadocs;
 
 /**
- * This class represents a shoe you can buy.
+ * This class represents a shoe you can buy. The price of the
+ * show must be positive.
  *
  * @author Josh Archer
- * @version
+ * @version 1.0
  */
-public class Shoe
+public class Shoe extends Object
 {
     //my shoes are interesting...
     private String brand;
@@ -14,19 +15,32 @@ public class Shoe
     private double price;
 
     /**
+     * Creates a new shoe with brand, name and price.
      *
-     * @param brand
-     * @param name
-     * @param price
+     * @param brand the shoe brand
+     * @param name the name
+     * @param price the price, which must be positive
+     *
+     * @throws IllegalArgumentException thrown when the price is negative
      */
     public Shoe(String brand, String name, double price)
     {
+        //defensive programming
+        if (price < 0)
+        {
+            throw new IllegalArgumentException("Negative price not supported");
+        }
+
         //assign my three fields
         this.brand = brand;
         this.name = name;
         this.price = price;
     }
 
+    /**
+     * Retrieves the shoe brand
+     * @return the shoe brand
+     */
     public String getBrand()
     {
         return brand;
@@ -43,8 +57,8 @@ public class Shoe
     }
 
     /**
-     *
-     * @return
+     * Returns the brand of the shoe as a string
+     * @return the brand name
      */
     @Override
     public String toString()
